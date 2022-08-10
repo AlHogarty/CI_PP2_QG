d714da1a3e6030671c2dfe43b3b44c27c6998602
Alan Hogarty <alanhogarty@gmail.com>
2022-08-10 18:39:16 +0000

    Add function to show question, Add first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,41 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(questions) {
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {
 
 }
 
-const questions =[
+const questions = [
     {
         question: 'What is 3 + 4',
         answers: [
-            { text: '4', correct: false},
-            { text: '7', correct: true},
-            { text: '34', correct: false},
-            { text: '6', correct: false},
+            { text: '4', correct: false },
+            { text: '7', correct: true },
+            { text: '34', correct: false },
+            { text: '6', correct: false }
         ]
     }
 ]
