5038c93f882b8e761d02319db6671eebe42dc336
Alan Hogarty <alanhogarty@gmail.com>
2022-08-10 18:16:41 +0000

    Link js to index page, add functionality to start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-function startGame() {
+const startButton = document.getElementById('start-btn')
 
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
 }
 
 function setNextQuestion() {
@@ -7,5 +12,5 @@ function setNextQuestion() {
 }
 
 function selectAnswer() {
-    
+
 }
